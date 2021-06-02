638a7f569ecab7ec34ab0b3cc57186f667aad881
Victor_vm9 <victorex.x@gmail.com>
2021-06-02 19:16:20 -0400

    Bajada informacion BD

--- proyectoTis2/src/pages/home/home.ts
+++ proyectoTis2/src/pages/home/home.ts
@@ -3,18 +3,36 @@ import { NavController } from 'ionic-angular';
 import { PublicacionPage } from '../publicacion/publicacion';
 import { FiltroPage } from '../filtro/filtro';
 
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  publicacionesDes:any;
+  id_publicacion:any;
+
+  constructor(public navCtrl: NavController, public http: Http) {
+
+    this.http.get('http://localhost/apiRest/public/publicacion')
+    .map(response => response.json())
+    .subscribe(data =>
+      {
+        this.publicacionesDes = data;
+        console.log(data);
+      },
+      err => {
+        console.log("Oops!");
+      }
+    );
 
   }
   
-  irPublicacion(){
-    this.navCtrl.push(PublicacionPage);
+  irPublicacion(id_publicacion){
+    this.navCtrl.push(PublicacionPage, {valor: id_publicacion});
   }
 
   irFiltro(){
--- proyectoTis2/src/pages/publicacion/publicacion.ts
+++ proyectoTis2/src/pages/publicacion/publicacion.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the PublicacionPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+//import { connectableObservableDescriptor } from 'rxjs/observable/ConnectableObservable';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
 
 @IonicPage()
 @Component({
@@ -15,11 +12,29 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class PublicacionPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  publicacion:any;
+  id_publicacion = this.navParams.get('valor');
+  data:Observable<any>;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
+
+    this.http.get('http://localhost/apiRest/public/publicacion/'+this.id_publicacion)
+    .map(response => response.json())
+    .subscribe(data =>
+      {
+        this.publicacion = data;
+
+        console.log(data);
+      },
+      err => {
+        console.log("Oops!");
+      }
+    );
+
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad PublicacionPage');
+    console.log('Ya cargó PublicacionPage');
   }
 
 }
