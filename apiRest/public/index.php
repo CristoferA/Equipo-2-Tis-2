b79552199fb38a0f06b495ac5838b671434f5174
Abysmal6 <41496239+Abysmal6@users.noreply.github.com>
2021-06-02 22:09:06 -0400

    aa

--- apiRest/public/index.php
+++ apiRest/public/index.php
@@ -1,4 +1,8 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true ");
+header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
+header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
 error_reporting(-1);
 ini_set('display_errors', 1);
 use Psr\Http\Message\ResponseInterface as Response;
