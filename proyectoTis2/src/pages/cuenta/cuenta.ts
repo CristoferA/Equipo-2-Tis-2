240ccbac941d1cb6f588e6af43e633c3d751af98
CristoferA <70931907+CristoferA@users.noreply.github.com>
2021-07-07 23:40:03 -0400

    Cuenta usuario

--- proyectoTis2/src/pages/cuenta/cuenta.ts
+++ proyectoTis2/src/pages/cuenta/cuenta.ts
@@ -2,16 +2,49 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { MisPublicacionesPage } from '../mis-publicaciones/mis-publicaciones';
 import { HomePage } from '../home/home';
-
+import { Observable } from 'rxjs';
+import { Http } from '@angular/http';
+import { LoginPage } from '../login/login';
 
 @IonicPage()
 @Component({
   selector: 'page-cuenta',
   templateUrl: 'cuenta.html',
 })
+
 export class CuentaPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  data:Observable<any>;
+  id_usuario:any;
+  nombre_usuario:any;
+  email_usuario:any;
+  cuenta: any;
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private http: Http) {
+
+
+    if('respuesta' in localStorage){
+    var respuesta = JSON.parse(localStorage.getItem('respuesta'));
+    var id_usuario = respuesta.data.id_usuario;
+    console.log(id_usuario);  
+
+    this.http.get('http://localhost/apiRest/public/usuario/'+id_usuario)
+    .map(response => response.json())
+    .subscribe(data => {
+      this.cuenta = data;
+      console.log(data);  
+    },
+    err => {
+      console.log("Oops!");
+    }
+  );
+   
+  }else{
+    this.irLogin();
+  }
+
   }
 
   ionViewDidLoad() {
@@ -21,14 +54,18 @@ export class CuentaPage {
   irMisPublicaciones(){
     this.navCtrl.push(MisPublicacionesPage);
   }
+
   irHome() {
     this.navCtrl.setRoot(HomePage);
+  }
+
+  irLogin() {
+    this.navCtrl.setRoot(LoginPage);
   }
 
   cerrar(){
       localStorage.clear();
       this.irHome();
   }
-
 
 }
