81594f2cff989bd33d943aab224a933c47cad393
VictoreX <victorex.x@gmail.com>
2021-07-28 22:19:18 -0400

    boton ordenar en publicaciones oferente

--- proyectoTis2/src/pages/publicaciones-oferente/publicaciones-oferente.ts
+++ proyectoTis2/src/pages/publicaciones-oferente/publicaciones-oferente.ts
@@ -39,6 +39,24 @@ export class PublicacionesOferentePage {
   irPublicacion(id_publicacion){
     this.navCtrl.push(PublicacionPage, {valor: id_publicacion});
   }
+
+  ordenarPubAsc($event){
+    if($event == "Asc"){
+      console.log($event);
+      //this.publicaciones.sort((a,b) => a.nombre_publicacion.localeCompare(b.nombre_publicacion));
+      this.publicaciones.sort((a,b) => a.nombre_publicacion.toLowerCase() < b.nombre_publicacion.toLowerCase() ? -1 : a.nombre_publicacion.toLowerCase() > b.nombre_publicacion.toLowerCase() ? 1 : 0);
+    }else if($event == "Des"){
+      console.log($event);
+      this.publicaciones.sort((a,b) =>  a.nombre_publicacion.toLowerCase() < b.nombre_publicacion.toLowerCase() ? 1 : a.nombre_publicacion.toLowerCase() > b.nombre_publicacion.toLowerCase() ? -1 : 0);   
+    }else if($event == "MenP"){
+      console.log($event);
+      this.publicaciones.sort((a,b) =>  a.valor_publicacion < b.valor_publicacion ? -1 : a.valor_publicacion > b.valor_publicacion ? 1 : 0);   
+    }else{
+      console.log($event);
+      this.publicaciones.sort((a,b) =>  a.valor_publicacion < b.valor_publicacion ? 1 : a.valor_publicacion > b.valor_publicacion ? -1 : 0);   
+    }
+
+  }
   
 
 
